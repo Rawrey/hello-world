40678aee7187f8a4480652f424541df59cf36804
Rawrey <Rawreygaming@gmail.com>
2017-05-13 15:22:52 +0200

    complete rework. added input field + button to add todos to the list.

--- src/todos.js
+++ src/todos.js
@@ -1,38 +1,16 @@
-// Tests for later use
-// want to outsource scripts here.
+// Create new entry
 
-<script>
-         <script>
-            var a ='';
-            var b ='';
-            var c ='';
-        </script>
-
-        <script type="text/javascript">
-            function show(){
-                alert ("Ihre ToDo's sind:" + a + ", " + b + " und " + c + ".");
+    function newElement(){
+        var li = document.createElement('li');
+        var input = document.getElementById("todoinput").value;
+        var add = document.createTextNode(input);
+    
+        li.appendChild(add);
+            if (input === '') {
+                alert('Bitte etwas eingeben.');
             }
-        </script>
-
-        <script type="text/javascript">
-            function news(){
-                alert ("Bitte geben sie ihre ToDo's ein!");
-                a = prompt("",a);
-                b = prompt("",b);
-                c = prompt("",c);
-                alert ("Ihre einträge wurden gespeichert.");
+            else {
+            document.getElementById('list').appendChild(li);
             }
-        </script>
+    }
 
-        <script type="text/javascript">
-            function edit(){
-                alert ('Not done yet');
-            }
-        </script>
-
-        <script type="text/javascript">
-            function delet(){
-                alert ('Not done yet');
-            }
-        </script>
-</script>
